3a08f26c45a61d32b1a83e4f61b155131b674f21
bmolina-nyc <bruce.molina.81@gmail.com>
2017-04-18 20:09:26 -0400

    Done.

--- components/Product.js
+++ components/Product.js
@@ -1,36 +1,35 @@
 const React = require('react');
 
-class Product extends React.Component {
-  render() {
-    return (
-      <div className="product">
-        <p>Name: {this.props.name}</p>
-        <p>Producer: {this.props.producer} </p>
-        <p>Watermark: {this.props.hasWatermark? 'Watermark' : 'No Watermark'} </p>
-        <p>Colors Available: {this.props.color} </p>
-        <p>Weight: {this.props.weight}</p>
+class Product extends React.Component{
+  render(){
+    return(
+      <div>
+        <p> Name: {this.props.name}  </p>
+        <p> Producer: {this.props.producer } </p>
+        <p> Watermark: {this.props.hasWatermark ? "Yes": "No Watermark"} </p>
+        <p> Color: {this.props.color} </p>
+        <p> Weight: {this.props.weight} </p>
       </div>
-    );
+    )
   }
 }
 
-
 Product.defaultProps = {
-  hasWatermark: false, 
-};
+  hasWatermark: false,
+
+}
+
 
 Product.propTypes = {
   name: React.PropTypes.string.isRequired,
   producer: React.PropTypes.string,
   hasWatermark: React.PropTypes.bool,
-  color: React.PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
-  weight: function(props, propName, componentName) {
-    const weight = props[propName];
-    console.log(props)  // the actual object 
-    console.log(propName) // the key (the props name)
-    console.log(componentName)  //the name of the component - this didn't need to be passed in this case 
+  color: React.PropTypes.oneOf(['white', 'eggshell-white','salmon']).isRequired,
+  weight: (props) => {
+  
+    const weight = props.weight 
 
-    if (weight === undefined) {
+  if (weight === undefined) {
       return new Error('The `weight` prop is required.');
     }
 
@@ -43,8 +42,9 @@ Product.propTypes = {
     if (!isValidWeight) {
       return new Error('The `weight` prop should range between 80 and 300.');
     }
-  },
-};
+    
+  }
+}
 
 
 module.exports = Product;
