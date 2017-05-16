c6d7d06e7bd0c580b6df492886b06b73d96afc3e
smithwebtek <brad@smithwebtek.com>
2017-05-15 21:12:51 -0400

    Done.

--- components/Product_solution.js
+++ components/Product_solution.js
@@ -1,43 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 export default class Product extends React.Component {
   render() {
     return (
       <div className="product">
-        <ul>
-          <li>{this.props.name}</li>
-          <li>{this.props.producer}</li>
-          <li>{this.props.hasWatermark ? 'Yes' : 'No'}</li>
-          <li>{this.props.color}</li>
-          <li>{this.props.weight}</li>
-          </ul>
+        <p>Name: {this.props.name}</p>
+        {this.props.producer ? <small>{this.props.producer}</small> : null}
+        <p>{this.props.hasWatermark ? 'Watermarked' : 'Not watermarked'}</p>
+        <p>Weight: {this.props.weight}</p>
       </div>
     );
   }
 }
 
 Product.defaultProps = {
-  hasWatermark: false
-	
+  hasWatermark: false,
 };
 
 Product.propTypes = {
   name: React.PropTypes.string.isRequired,
   producer: React.PropTypes.string,
   hasWatermark: React.PropTypes.bool,
-  color: React.PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,	
-	weight: (props, propName) => {
+  color: React.PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: (props, propName) => {
     const weight = props[propName];
+
     if (weight === undefined) {
       return new Error('The `weight` prop is required.');
     }
+
     if (isNaN(weight)) {
       return new Error('The `weight` prop is not a number.');
     }
+
     const isValidWeight = weight > 80 && weight < 300;
+
     if (!isValidWeight) {
       return new Error('The `weight` prop should range between 80 and 300.');
     }
-  }
-}
+  },
+};
