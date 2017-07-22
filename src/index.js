8e6e8df00fe12c1cd30052c695a5fb15392039d1
Ivan Pilot <ivanpilot@gmail.com>
2017-07-22 16:38:27 +0200

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 import Product from './components/Product';
 
 ReactDOM.render(
-  <Product 
-    name="Dunder Mifflin" 
-    producer="PaperCo" 
-    color="white" 
+  <Product
+    name="Dunder Mifflin"
+    producer="PaperCo"
+    color="white"
     weight={210} 
   />,
   document.getElementById('root')
