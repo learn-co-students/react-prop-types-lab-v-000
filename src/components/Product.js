9f00e821213ecc374ad9e9ecf9fa1bd8a54895e7
AJ <ajoeford@gmail.com>
2017-07-24 15:22:08 -0700

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,78 @@
-// Code Product Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Name: {this.props.name}</li>
+          <li>Producer: {this.props.producer}</li>
+          <li>Watermark: {this.props.hasWatermark ? "yes" : "no"}</li>
+          <li>Color: {this.props.color}</li>
+          <li>Weight: {this.props.weight}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false,
+}
+
+var weightCheck = createChainableTypeChecker(weightRange);
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white','eggshell-white','salmon']).isRequired,
+  weight: weightCheck.isRequired
+}
+
+// custom proptype
+
+function weightRange(props, propName, componentName) {
+  componentName = componentName || 'ANONYMOUS';
+
+  let value = props[propName];
+  if (typeof value === 'number') {
+    if(value > 80 && value < 300){
+      return null;
+    }
+    else {
+      console.error(propName + 'in ' + componentName + " must be between 80 and 300.");
+    }
+  }
+  else {
+    console.error(propName + 'in ' + componentName + " must be number between 80 and 300.");
+  }
+
+}
+
+function createChainableTypeChecker(validate) {
+  function checkType(isRequired, props, propName, componentName, location) {
+    componentName = componentName || ANONYMOUS;
+    if (props[propName] == null) {
+      var locationName = ReactPropTypeLocationNames[location];
+      if (isRequired) {
+        return new Error(
+          ("Required " + locationName + " `" + propName + "` was not specified in ") +
+          ("`" + componentName + "`.")
+        );
+      }
+      return null;
+    } else {
+      return validate(props, propName, componentName, location);
+    }
+  }
+
+  let chainedCheckType = checkType.bind(null, false);
+  chainedCheckType.isRequired = checkType.bind(null, true);
+
+  return chainedCheckType;
+}
+
+export default Product;
