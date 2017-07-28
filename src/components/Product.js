a82f82f32edd6967f078187bc4c415a970da749e
Isis Tejeda <isis@isistejeda.com>
2017-07-27 20:04:37 -0500

    Before fix

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,40 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+export default class Product extends React.Component {
+  render() {
+    return (
+      <div className="ship">
+          <p>{this.props.name} </p>
+          <p>{this.props.producer}</p>
+          <p>{this.props.hasWatermark}</p>
+          {/*<p>{this.props.color.join(', ')}</p>*/}
+          <p>{this.props.weight}</p>
+
+      </div>
+    );
+  }
+}
+//default
+Product.defaultProps = {
+  hasWatermark: false,
+};
+
+Product.propTypes = {
+    producer: PropTypes.string,
+    
+    hasWatermark: PropTypes.bool,
+    // color: PropTypes.arrayOf(PropTypes.string).isRequired,
+    name: PropTypes.string.isRequired,
+    weight: isWeight
+
+};
+
+function isWeight(props, weight, components){
+    if (80 > weight < 300 ){
+        return 'must be between 80 & 300';
+    }
+}
+
+
