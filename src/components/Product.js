6033b1910474fe888e87877a4fbb197e42af8a3d
Pamela <github email address>
2017-09-25 18:18:40 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -19,11 +19,13 @@ var weight_range = [];
 for (var i = 80; i <= 300; i++) {                                        weight_range.push(i);
 }
 
+var colors = ['white', 'eggshell-white', 'salmon']
+
 Product.propTypes = {
   name: PropTypes.string.isRequired,
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
-  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  color: PropTypes.oneOf(colors).isRequired,
   weight: PropTypes.oneOf(weight_range).isRequired
 }
 
