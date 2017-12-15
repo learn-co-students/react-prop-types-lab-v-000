d13f20bc71ba6f70e4d2389b99ba102a95521d57
Sophia Hubeny <shhubeny@gmail.com>
2017-12-15 23:03:58 +0000

    progress

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,29 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>{this.props.name} </h1>
+        <h3>{this.props.producer} </h3>
+        <h3>{this.props.hasWatermark} </h3>
+        <h3>{this.props.color.join(', ')} </h3>
+        <h3>{this.props.weight} </h3>
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.arrayOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: PropTypes.number
+};
