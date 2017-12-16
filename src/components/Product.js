c38e5ebf0901fabf1ccff59aebbc761101a2fa12
Sophia Hubeny <shhubeny@gmail.com>
2017-12-16 06:20:42 +0000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,14 +3,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Product extends React.Component {
-  render(){
+  render() {
     return(
       <div>
-        <h1>{this.props.name} </h1>
-        <h3>{this.props.producer} </h3>
-        <h3>{this.props.hasWatermark} </h3>
-        <h3>{this.props.color.join(', ')} </h3>
-        <h3>{this.props.weight} </h3> //need to fix for custom validation - between 80 - 300
+        Product
       </div>
     )
   }
@@ -24,6 +20,18 @@ Product.propTypes = {
   name: PropTypes.string.isRequired,
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
-  color: PropTypes.arrayOf(['white', 'eggshell-white', 'salmon']).isRequired, // need to fix for array
-  weight: PropTypes.number
+  color: PropTypes.oneOf([
+    'white',
+    'eggshell-white',
+    'salmon'
+  ]).isRequired,
+  customProp: function(props, weight, Product) {
+    if (props[weight] > 300 || props[weight] < 80) {
+      return new Error(
+        'Invalid prop `' + weight + '`. Validation failed.'
+      );
+    }
+  },
 };
+
+export default Product;
