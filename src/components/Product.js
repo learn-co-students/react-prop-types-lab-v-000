0c2b01a96191477a00559b38635eb8d57d3f0210
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-01-27 22:10:28 +0000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,53 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types'; 
+
+class Product extends React.Component {
+ 
+  render() {
+    return (
+        <div> 
+            <h2>{this.props.name}</h2>  
+            <h2>{this.props.producer}</h2>  
+            <h2>{this.props.hasWatermark}</h2>  
+            <h2>{this.props.color}</h2>  
+            <h2>{this.props.weight}</h2> 
+        </div> 
+    )
+  } 
+}; 
+
+Product.defaultProps = {
+  hasWatermark: false
+}; 
+
+
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: function(props, propName, componentName) {
+    if (props[propName] == undefined) {
+      return new Error(
+        'Invalid prop `' + propName + '` supplied to' +
+        ' `' + componentName + '`. Validation failed.'
+      );
+    } 
+    // console.log(props) 
+    // console.log(typeof(props[propName])!== 'number')
+     if (isNaN(props[propName])) {
+      return new Error(
+        'Weight is not a number.'
+      );
+    }
+    
+    if (props[propName] <= 80 || props[propName] >= 300) { 
+        return new Error( 
+            `Weight is out of range.`
+        );
+    }  
+  }
+};
+            
+export default Product;
