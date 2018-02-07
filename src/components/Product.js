6fa561df1de61cd80c8a9535c9e211a43895708f
Bob Habig <rfhab2@gmail.com>
2018-02-07 15:53:47 -0500

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,45 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Product = (props) => {
-  return(
-    <div>
-      <p><strong>Product Name:</strong> {props.name}</p>
-      <p><strong>Producer:</strong> {props.producer}</p>
-      <p><strong>Water Marked?:</strong> {props.hasWatermark === true ? 'YOU KNOW IT' : 'Sorry, but it is cheap...'}</p>
-      <p><strong>Color:</strong> {props.color}</p>
-      <p><strong>Weight:</strong> {props.weight}</p>
-    </div>
+class Product extends React.Component {
+  render() {
+  return (
+    <div></div>
   )
 }
-
-const rigthWeight = (props, propName, componentName) => {
-  componentName = componentName || "ANONYMOUS";
-
-  if (props[propName]) {
-    let value = props[propName];
-
-    if (typeof value !== 'number') {
-      return new Error(`${propName} in ${componentName} must be a number.`);
-    } else if (value < 80 || value > 300) {
-      return new Error(`${propName} in ${componentName} must be between 80 and 300 (lbs).`);
-    }
-  } else {
-    return new Error(`${propName} in ${componentName} is required.`);
-  }
-  return null;
 }
 
 Product.defaultProps = {
   hasWatermark: false,
-};
+}
+
+const weightValidator = (props, propName, componentName) => {
+  componentName = componentName || 'ANONYMOUS';
+  if (props[propName]) {
+    let value = props[propName];
+    if (typeof value === 'number') {
+      if (value <= 80) {
+        return new Error(propName + ' in ' + componentName + " is less than 80, but must be within 80 and 300.");
+      } else if (value >= 300) {
+        return new Error(propName + ' in ' + componentName + " is greater than 300, but must be within 80 and 300.");
+      }
+    } else {
+      return new Error(propName + ' in ' + componentName + " must be a number");
+    }
+  } else {
+    return new Error('weight' + ' in ' + componentName + " is required.");
+  }
+}
 
 Product.propTypes = {
   name: PropTypes.string.isRequired,
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
-  weight: rigthWeight,
-};
+  weight: weightValidator
+}
 
 export default Product;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Product from './components/Product';
 
 ReactDOM.render(
-  <Product
-    name="Dunder Mifflin"
-    producer="PaperCo"
-    color="white"
-    weight={210}
-  />,
+  <Product />,
   document.getElementById('root')
-);
+)
