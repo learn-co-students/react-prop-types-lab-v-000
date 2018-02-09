060ba4f919d363b29a9c5187c998747a0c955c1b
emorgan05 <emorgan05@gmail.com>
2018-02-09 17:04:04 -0500

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,73 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div className="product">
+        <h1>{this.props.name}</h1>
+        <ul>
+          <li>{this.props.producer}</li>
+          <li>{this.props.hasWatermark}</li>
+          <li>{this.props.color}</li>
+          <li>{this.props.weight}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false,
+};
+
+let weightNumber = createChainableTypeChecker(weightChecker);
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: weightChecker.isRequired
+}
+
+function weightChecker(props, propName, componentName, location) {
+  componentName = componentName || 'ANONYMOUS';
+  if (props[propName]) {
+    let value = props[propName];
+    if (typeof value === 'number') {
+      if (value > 80 && value < 300) {
+        return null;
+      } else {
+        new Error(propName + ' in ' + componentName + 'is not in the proper range');
+      }
+    }
+  }
+  return null;
+}
+
+function createChainableTypeChecker(validate) {
+  function checkType(isRequired, props, propName, componentName, location) {
+    componentName = componentName || ANONYMOUS;
+    if (props[propName] == null) {
+      var locationName = ReactPropTypeLocationNames[location];
+      if (isRequired) {
+        return new Error(
+          ("Required " + locationName + " `" + propName + "` was not specified in ") +
+          ("`" + componentName + "`.")
+        );
+      }
+      return null;
+    } else {
+      return validate(props, propName, componentName, location);
+    }
+  }
+
+  let chainedCheckType = checkType.bind(null, false);
+  chainedCheckType.isRequired = checkType.bind(null, true);
+
+  return chainedCheckType;
+}
+
+export default Product;
