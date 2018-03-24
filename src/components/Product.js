ab7e7c6d6034ae9c65bd63e3f0c99385d1c162e1
Sara Tibbetts <sxt4866@rit.edu>
2018-03-24 17:55:30 -0400

    finishes lab

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,49 @@
-// Code Product Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+
+class Product extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <ul>
+          <li>Producer: {this.props.producer}</li>
+          <li>Watermarked: {this.props.hasWatermark.toString()}</li>
+          <li>Color: {this.props.color}</li>
+          <li>Weight: {this.props.weight}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false,
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: weightOptions
+}
+
+function weightOptions(props, propName, componentName){
+  componentName = componentName || 'ANONYMOUS';
+
+  if(props[propName]){
+    let value = props[propName];
+    if(typeof value === 'number'){
+      return (value >= 80 && value <= 300) ? null : new Error(propName + 'in ' + componentName + " is not between 80 and 300.");
+    } else {
+      return new Error(propName + 'in ' + componentName + " must be a number");
+    }
+  }
+
+  return new Error(propName + 'in ' + componentName + " is required");
+}
+
+export default Product;
