3bace66c79d3479fbe2e58a59f145bd728c91d15
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-04-14 22:14:29 +0000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,59 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
+    render() {
+        return (
+            <div className="product">
+                <ul>
+                  <li>{this.props.name}</li>
+                  <li>{this.props.producer}</li>
+                  <li>{this.props.hasWatermark}</li>
+                  <li>{this.color}</li>
+                  <li>Weight: {this.props.weight}</li>
+                </ul>
+             </div>
+    
+        );
+    }
+}
+
+Product.defaultProps = {
+  hasWatermark: false,
+  
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  
+  weight: function(props, propName) {
+    const weight = props[propName]      
+    
+    if (weight === undefined) {
+        return new Error('Weight is required')
+    }
+    
+    if (isNaN(weight)) {
+        return new Error('Weight must be a number')
+    }
+    
+    if(weight < 80 || weight > 300) {
+        return new Error('Invalid');
+   }
+  },
+//   weight: PropTypes.number.isRequired
+  
+};
+
+export default Product;
+// completed: function(props, propName, componentName) {
+//   if(props[propName] < MIN || props[propName] > MAX) {
+//       return new Error('Invalid');
+//   }
+// }
+
+
