c55715142462ee25a099d2c7c2addd310200db37
mbalsamo <github email address>
2018-05-07 14:50:59 -0400

    done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,34 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+let weight_range = [];
+for (var i = 80; i <= 300; i++) {
+  weight_range.push(i);
+}
+
+
+export default class Product extends React.Component {
+  render() {
+    return (
+      <div className="product">
+        <h1>{this.props.name}</h1>
+        <p>{this.props.producer}</p>
+        <p>{this.props.hasWatermark}</p>
+        <p>{this.props.color}</p>
+        <p>{this.props.weight}</p>
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: PropTypes.oneOf(weight_range).isRequired
+}
