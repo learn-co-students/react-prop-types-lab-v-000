e823e51c8733d24c88ebe94b36fdc27967d95f5e
github username apalski <github email address annette.drapalski@gmail.com>
2018-05-09 08:13:27 +1000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,43 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types'
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.name}</p>
+        <p>{this.props.producer}</p>
+        <p>{this.props.hasWatermark}</p>
+        <p>{this.props.color}</p>
+        <p>{this.props.weight}</p>
+      </div>
+    );
+  }
+}
+
+function weightPropType(props, propName) {
+  var weight = props[propName];
+  if (weight === undefined) {
+    return new Error('weight undefined');
+  }
+  if(isNaN(weight)) {
+    return new Error('weight must be a number');
+  }
+  if (weight < 80 || weight > 300) {
+    return new Error('Must be between 80 and 300');
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: weightPropType
+};
+
+export default Product;
