b870cf2aebb2e95e4a1cc9b7dc182df2ef1f40b3
lawrend <douglas_lawrence@mac.com>
2018-05-27 19:20:49 -0700

    done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,50 @@
-// Code Product Component Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+class Product extends React.Component {
+    render () {
+        return (
+            <div className="product">
+            <p>Name: {this.props.name}</p>
+            <p>Producer: {this.props.producer}</p>
+            <p>Watermark: {this.props.hasWatermark ? 'Yes' : 'No' }</p>
+            <p>Color: {this.props.color}</p>
+            <p>Weight: {this.props.weight}</p>
+            </div>
+        )
+    }
+};
+
+
+function customWeight(props, propName, componentName) {
+     if (!(props[propName])) {
+        return new Error(
+            'Prop `' + propName + '` missing from ' + ' `' + componentName + '`. Validation failed.'
+            );
+     } else if ( typeof (props[propName]) !== 'number' ) {
+        return new Error(
+            'Invalid prop `' + propName + '` is not a number and supplied to' + ' `' + componentName + '`. Validation failed.'
+            );
+        } else if ( props[propName] < 81 || props[propName] > 299 ) {
+        return new Error(
+            'Invalid prop `' + propName + '` supplied to' + ' `' + componentName + '`. Validation failed.'
+            );
+        }
+};
+
+Product.defaultProps = {
+    hasWatermark: false,
+};
+
+
+
+Product.propTypes = {
+    name: PropTypes.string.isRequired,
+    producer: PropTypes.string,
+    hasWatermark: PropTypes.bool,
+    color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+    weight: customWeight
+}
+
+export default Product;
