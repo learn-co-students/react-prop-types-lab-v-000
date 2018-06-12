85211decac25863971b95003f9d2f29e5873a420
github username <github email address>
2018-06-12 11:14:39 +0300

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,15 +3,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Product extends React.Component {
-  render(){
-    return(
-      <div className="product">
-        <h1> Product Name: {this.props.name} </h1>
-        <p> Producer: {this.props.producer} </p>
-        <p> {this.props.hasWatermark ? 'Yes' : 'No'} </p>
-        <p> Color: {this.props.color} </p>
-        <p> Weight: {this.props.weight} </p>
-      </div>
+  render() {
+    return (
+      <div>Product</div>
     )
   }
 };
@@ -25,7 +19,7 @@ Product.propTypes = {
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white','eggshell-white','salmon']).isRequired,
-  weight: function(props, propName, componentName) {
+  weight: (props, propName) => {
     let weight = props[propName];
 
     if (weight === undefined) {
