861affdca311829c9076a88e7e44c99756496bce
kostanzhoglo <jeffreywithers@gmail.com>
2018-06-13 20:58:35 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,45 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Product extends React.Component {
+  render() {
+    const { name, producer, hasWatermark, color, weight } = this.props;
+
+    return (
+      <div className="productInfo">
+        <ul>
+          <li>Name: {name}</li>
+          <li>Producer: {producer}</li>
+          <li>Has a Watermark: {hasWatermark ? "Yes" : "No"}</li>
+          <li>Color: {color}</li>
+          <li>Weight: {weight}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: function(props, propName, componentName) {
+    const weight = props[propName];
+    if (typeof weight === 'number') {
+      const validWeight = 80 < weight && weight < 300;
+      if (!validWeight) {
+        return new Error('Weight value must be between 80 and 300.')
+      }
+    } else if (typeof weight !== 'number' && typeof weight !== 'undefined') {
+      return new Error('Weight value must be a number')
+    } else {
+      return new Error('Weight is required.')
+    }
+  }
+}
