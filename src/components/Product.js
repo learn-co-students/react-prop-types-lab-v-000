3baa56d363e8c2694880edc472398be7ea45e3cf
Eden <emesfun@outlook.com>
2018-06-24 15:22:34 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,15 @@ import PropTypes from 'prop-types';
 export default class Product extends React.Component {
     render() {
         return (
-            console.log("Hello!")
+            <div>
+                <ul>
+                <li>{this.props.name}</li>
+                <li>{this.props.producer}</li>
+                <li>{this.props.hasWatermark}</li>
+                <li>{this.props.color}</li>
+                <li>{this.props.weight}</li>
+                </ul>
+            </div>
         )
     }
 }
