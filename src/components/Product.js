12a1410c651a135235ba6031d05ac38526359b09
coryjbergquist <cory.j.bergquist@gmail.com>
2018-09-25 11:58:52 -0700

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,31 @@
 // Code Product Component Here
+import React from "react"
+import PropTypes from 'prop-types';
+
+export default class Product extends React.Component {
+  render () {
+    return (
+      <div>
+      { this.props.name }
+      { this.props.producer }
+      { this.props.hasWatermark }
+      { this.props.color }
+      { this.props.weight }
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {hasWatermark: false}
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(["white", "eggshell-white", "salmon"]).isRequired,
+  weight: function(props, propName, componentName) {
+    const weight = props[propName];
+    if (typeof weight !== 'number' || weight > 300 || weight < 80) {
+      console.error('Invalid ' + propName + 'supplied to ' + componentName + '. Validation failed.');
+    }
+  }
+}
