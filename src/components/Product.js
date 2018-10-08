6414ff7f71a9ae674941a81e2c135a89d0f47e8e
Suchitra Naik <susanaik@gmail.com>
2018-10-08 19:21:00 +0000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,36 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Product extends React.Component {
+
+  render() {
+    return (
+      <div className="product">
+        <h1>Product Name: {this.props.name}</h1>
+        <p>Producer: {this.props.producer}</p>
+        <p>Color: {this.props.color}</p>
+        <p>Weight: {this.props.weight}</p>
+      </div>
+    );
+  }
+}
+
+Product.defaultProps = {
+  name: "Dunder Mifflin",
+  producer: "PaperCO",
+  hasWatermark: true,
+  color: ['white', 'eggshell-white' ,'salmon'],
+  weight: [80,300]
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+
+  color: PropTypes.arrayOf(PropTypes.string).isRequired,
+  weight: PropTypes.objectOf(PropTypes.number).isRequired,
+
+
+};
