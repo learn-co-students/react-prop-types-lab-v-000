9bc192ae423277e0e227f9a5fb3353f9ddd85c4a
Su0414 <susanaik.com>
2018-10-11 10:40:49 -0700

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,36 +1,45 @@
-// Code Product Component Here
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Product extends React.Component {
+class Product extends React.Component {
 
   render() {
     return (
-      <div className="product">
-        <h1>Product Name: {this.props.name}</h1>
-        <p>Producer: {this.props.producer}</p>
-        <p>Color: {this.props.color}</p>
-        <p>Weight: {this.props.weight}</p>
-      </div>
-    );
+      <div>Product</div>
+    )
   }
 }
 
 Product.defaultProps = {
-  name: "Dunder Mifflin",
-  producer: "PaperCO",
-  hasWatermark: true,
-  color: ['white', 'eggshell-white' ,'salmon'],
-  weight: [80,300]
-};
+  hasWatermark: false
+}
 
 Product.propTypes = {
   name: PropTypes.string.isRequired,
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf([
+    'white', 
+    'eggshell-white', 
+    'salmon'
+  ]).isRequired,
+  weight: (props, propName) => {
+    const weight = props[propName];
 
-  color: PropTypes.arrayOf(PropTypes.string).isRequired,
-  weight: PropTypes.objectOf(PropTypes.number).isRequired,
+    if (weight === undefined) {
+      return new Error('The `weight` prop is required.');
+    }
 
+    if (isNaN(weight)) {
+      return new Error('The `weight` prop is not a number.');
+    }
 
-};
+    const isValidWeight = weight > 80 && weight < 300;
+
+    if (!isValidWeight) {
+      return new Error('The `weight` prop should range between 80 and 300.');
+    }
+  },
+}
+
+export default Product;
