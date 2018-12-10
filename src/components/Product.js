49c212b1ec01515e5f6a51b440a33d0b34586aec
terryblue99 <terryblue@comcast.net>
2018-12-10 17:49:36 -0600

    initial code

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,17 @@
-// Code Product Component Here
+import React from 'react'
+
+export default class Product extends React.Component {
+	render() {
+	    return (
+	      <div className="product">
+	        <ul>
+	          <li>Name: {this.props.name}</li>
+	          <li>Producer: {this.props.producer}</li>
+	          <li>Has Watermark?: {this.props.hasWatermark ? 'Yes' : 'No'}</li>
+	          <li>Color: {this.props.color}</li>
+	          <li>Weight: {this.props.weight}</li>
+	        </ul>
+	      </div>
+	    );
+  	}
+}
