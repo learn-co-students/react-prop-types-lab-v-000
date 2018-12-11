981800a551bb560ed362827d81d990463ca4c2b1
terryblue99 <terryblue@comcast.net>
2018-12-11 13:38:54 -0600

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+function weightRangeCheck(props, propName, componentName) {
+    let weight = props[propName];
+    
+    if (weight === undefined) {
+      return new Error("Sorry, you must include a number for weight!");
+    }
+
+    if (isNaN(weight)) {
+      return new Error("Sorry, weight must be a number!");
+    }
+
+    return weight >= 80 && weight <= 300 ? null : new Error("Sorry, weight must be within the range of 80 to 300!");
+  } 
 
 export default class Product extends React.Component {
 	render() {
@@ -15,3 +30,15 @@ export default class Product extends React.Component {
 	    );
   	}
 }
+
+Product.defaultProps = {
+	hasWatermark: false
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,		
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: weightRangeCheck
+} 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
 import Product from './components/Product'
-
-function weightRangeCheck(props, propName, componentName) {
-    let weight = props[propName];
-    
-    if (weight === undefined) {
-      return new Error("Sorry you must include a number for weight.");
-    }
-
-    if (isNaN(weight)) {
-      return new Error("Sorry weight must be a number.");
-    }
-
-    return weight >= 80 && weight <= 300 ? null : new Error("Must be within range of 80 to 300");
-  }
-
-Product.defaultProps = {
-	hasWatermark: false
-}
-
-Product.propTypes = {
-  name: PropTypes.string.isRequired,
-  producer: PropTypes.string,		
-  hasWatermark: PropTypes.bool,
-  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
-  weight: weightRangeCheck
-}  
 
 ReactDOM.render(
   <Product
